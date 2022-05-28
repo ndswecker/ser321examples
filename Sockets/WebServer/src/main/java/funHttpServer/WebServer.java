cd0920c855105781fea465b93b4bfb3326e668ee
Nathanael Swecker <nswecker@asu.edu>
2022-05-28 11:25:13 -0700

    ln 282 attempt to print full json.

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -279,7 +279,7 @@ class WebServer {
           builder.append("HTTP/1.1 200 OK\n");
           builder.append("Content-Type: text/html; charset=utf-8\n");
           builder.append("\n");
-          builder.append("Check the todos mentioned in the Java source file");
+          builder.append("Check the todos mentioned in the Java source file" + json);
           // TODO: Parse the JSON returned by your fetch and create an appropriate
           // response based on what the assignment document asks for
 
